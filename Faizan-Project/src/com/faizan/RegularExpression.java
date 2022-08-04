c6fc6de8f59d425af8a20a69856f2016c67f7f06
faizansopariwala <faizansopariwala@EZDI-LAP0089>
2022-08-04 18:56:29 +0530

    Please consider the change

---
+++ Faizan-Project/src/com/faizan/RegularExpression.java
@@ -0,0 +1,14 @@
+package com.faizan;
+
+public class RegularExpression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="a";
+		
+		System.out.println(s.matches("\\D"));
+
+	}
+
+}
